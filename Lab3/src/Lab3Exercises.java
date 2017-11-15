08082a94a44e44708cc8c8b462d790c8e235fcd5
4015b7 <4015b7@jobo.ece.uprm.edu>
2017-11-15 18:35:59 -0400

    added a method to support a new class

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,8 +5,11 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Horeb Cotto Rosado");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(500, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 		
+				
 	}
 }
